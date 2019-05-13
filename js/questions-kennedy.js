a4622a11e06a40ca79dbf0588f5b3717b1450eea
Kennedy <kcpew@students.nic.edu>
2019-05-13 12:05:48 -0700

    quiz.html page
    Created new html document in order to build skelton for the questions page that will be populated by js

---
+++ js/questions-kennedy.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+
+  var questionArea = document.getElementsByClassName('questions')[0],
+
+       answerArea  = document.getElementsByClassName('answers')[0],
+
+var allQuestions = [
+  "Which of these four is your favorite arcade game?",
+  "What is your favorite movie out of these four?",
+  "What lyrics resonate best with you?",
+  "We gotta know - what's your favorite neon color?",
+  "Who is your 80's icon?",
+  "Who do you think is the better singer?";
+];
+
+
+
+};
